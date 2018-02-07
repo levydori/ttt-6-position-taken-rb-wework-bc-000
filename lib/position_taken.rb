a79d80654f4d4a63021199a90ea3daf7c4ad0369
Dori Levy <dori.levy@wework.com>
2018-02-07 02:34:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, pos)
-  empty_spaces = ["", " "]
+  empty_spaces = ["", " ", nil]
   return !empty_spaces.include?(array[pos])
 end
